8d7c27800230ab60107ab587fea3e9b8aa50224d
Ashley Degollado <ashley.degollado@student.hhs.school.nz>
2026-03-30 12:16:53 +1300

    beginning to add enemy class

---
+++ enemy.py
@@ -0,0 +1,16 @@
+import pygame
+
+class Enemy():
+    def __init__(self, x, y, w, h, [])
+
+    def images():
+
+#collide()
+#position()
+#size()
+#move()
+#color()
+#draw surface()
+#direction()
+#size(h, w)
+#vector(speed, direction)
